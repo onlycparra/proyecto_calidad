f794bd12ed24a47d3482d2b59d3f93fda5720d7f
Claudio Parra <onlycparra@gmail.com>
2014-12-12 11:51:09 -0300

    Creado el programa principal

---
+++ programita.c
@@ -0,0 +1,24 @@
+/**********************************************************************
+filename:		xxxx.c
+Program Name:	xxxx
+Version:		1
+By:				Claudio Parra
+email:			onlycparra@gmail.com
+
+Description:	xxxxxxx
+**********************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(int *argc,char *argv[]){
+	if (a){
+		asas;
+	}else{
+		sdasd;
+		funcion();
+	}
+
+
+
+	return;
+}
